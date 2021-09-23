3709c26b1445746957298cd376f65d1105d1a410
sohakhan77 <sohakhank73@gmail.com>
2021-09-23 09:53:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+//write a program which shows the counter after each second.
+ 
+//  timer = () => {
+//     let date = new Date();
+//     let hours = date.getHours();
+//     let min = date.getMinutes();
+//     let sec = date.getSeconds();
+//     console.log(`${hours}: ${min}: ${sec}`);
+// }
+// setInterval(timer,1000)
+
+// write a function which takes your name and displays the greeting
+// with your name
+
+// let userName = prompt("Please! Enter your name");
+// greet = (a) => {
+//     console.log(`Good Morning! ${a}`);
+// }
+// greet(userName)
+
+// write a function(arrow function) which takes two values and return
+// its sum as a result
+
+// let num1 = +prompt("Enter first number");
+// let num2 = +prompt("Enter second number");
+// addition = (a,b) => console.log(`${a} + ${b} = ${a+b}`);
+// addition(num1,num2)
+
+//write a function(arrow function) which takes a number and multiply it
+// with 0.5 and return the new value. print the new value outside the function
+
+// let num1 = +prompt("Enter first number");
+// multiplication = (a,b) => `${a} x 0.5 = ${a*b}`
+// console.log(multiplication(num1,0.5));
+
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+
+// let arr = [1,2,3,4,5]
+// arr.map((i)=> console.log(i))
+
+// let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},
+//{id:3,name:"xyz"}] iterate the given array through map function and
+// print the name and id
+
+// let arr = [
+//     {id:1,name:"abc"},
+//     {id:2,name:"efg"},
+//     {id:3,name:"hij"},
+//     {id:4,name:"xyz"}
+// ]
+
+//     arr.map((i) => console.log(`${i.id} ${i.name}`))
